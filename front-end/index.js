5560cc670aeb405592c8eba4621eab868bf8d883
Paul Hartman <paul.hartman@stickergiant.com>
2022-05-03 16:07:35 -0600

    just do it

--- front-end/index.js
+++
@@ -1,9 +0,0 @@
-
-    document.getElementById('profile-img').addEventListener('click', function() {
-        window.location.href = "./test.html"
-    });
-
-    function go() {
-        window.location.href = "./admin/manage.html"
-    }
-
